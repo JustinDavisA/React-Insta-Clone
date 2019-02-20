6c00540ee23e690f2137fdca83ec0f4e2d305a14
justindavisa <justindavisa@gmail.com>
2019-02-20 16:25:36 -0500

    Post.js refactor complete

--- instagram/src/components/Post/Post.js
+++ instagram/src/components/Post/Post.js
@@ -1,79 +1,33 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import CommentLogo from '../../assets/camera.svg'
-import HeartLogo from '../../assets/heart.svg'
-import './PostContainer.css';
-import CommentSection from '../CommentSection/CommentSection';
+import './Post.css';
 
-function PostContainer(props) {
+const Post = props => {
     return (
-        <>
-            {props.posts.map(post => 
-            <div key={post.timestamp} className='post-container'>
-                <div className='post-head'>
-                    <img width='40' height='40' className='user-image' src={post.thumbnailUrl} />
-                    <h2>{post.username}</h2>
-                </div>
-
-                <div className='post-image'>
-                    <img src={post.imageUrl} />
-                </div>
-
-                <div className='post-comment-section'>
-                    <div className='post-icons'>
-                        <img width='40' height='40' src={HeartLogo} className='heart-icon' />
-                        <img width='40' height='40' src={CommentLogo} className='comment-icon' />
-                    </div>
-
-                    <div className='post-likes'>
-                        <p><strong>{post.likes} likes</strong></p>
-                    </div>
-
-                    <div className='post-comments'>
-                        {post.comments.map((comment, i ) => (
-                            <Fragment key={i}>
-                                <p><strong>{comment.username} </strong>{comment.text}</p>      
-                            </Fragment>
-                        ))}
-                    </div>
-
-                    <div className='post-date'>
-                        <h3>{post.timestamp}</h3>
-                    </div>
-
-                    <div className='post-new-comment'>
-                        <input className='comment-input' placeholder='Add a comment...'></input>
-                        <div className='comment-submit'>
-                            <strong>...</strong>
-                        </div>
-                    </div>
-                </div>
+        <div className='post-container'>
+            <div className='post-head'>
+                <img 
+                    width='40' 
+                    height='40' 
+                    className='user-image' 
+                    src={props.posts.thumbnailUrl} 
+                />
+                <h2>{props.posts.username}</h2>
             </div>
-            )}
-        </>
+
+            <div className='post-image'>
+                <img src={props.posts.imageUrl} />
+            </div>
+        </div>
     );
 }
 
 PostContainer.propTypes = {
-    postContainer: PropTypes.arrayOf(
-        PropTypes.shape({
-            username: PropTypes.string.isRequired,
-            thumbnailUrl: PropTypes.string.isRequired,
-            imageUrl: PropTypes.string.isRequired,
-            likes: PropTypes.number.isRequired,
-            timestamp: PropTypes.string.isRequired,
-            comments: PropTypes.arrayOf(
-                PropTypes.shape({
-                    username: PropTypes.string.isRequired,
-                    text: PropTypes.string.isRequired
-                })
-            )
-        })
-    )
-}
-
-PostContainer.defaultProps = {
-    postContainer: []
+    PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        thumbnailUrl: PropTypes.string.isRequired,
+        imageUrl: PropTypes.string.isRequired,
+    })
 }
 
 export default PostContainer;
